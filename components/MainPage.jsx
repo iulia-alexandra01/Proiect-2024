91991fe00bf43cf9b2dda0b342d69a7e4a10d9f7
iulia-alexandra01 <iulia.alexandra1812@gmail.com>
2024-05-10 20:01:42 +0300

    Proiect final

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -84,7 +84,7 @@ const MainPage = () => {
           </div>
         ))}
       </div>
-      <div class="max-w-3xl mx-auto text-center mt-16">
+      <div className="max-w-3xl mx-auto text-center mt-16">
               <button
                 type="button"
                 className="class=mb-4 text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
